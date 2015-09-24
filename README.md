c1bfb23978613522731a24056d3e14e84f175268
Hoàng Rio <donghoang.nguyen@outlook.com>
2015-09-24 15:58:58 +0700

    Dang script len

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# MessageBox.js
-Hộp thoại thông báo cho web
+<meta charset="UTF_8">
+<h1># MessageBox.js
+Hộp thoại thông báo cho web</h1>
+<h2>Hướng dẫn sử dụng:</h1>
+<ol>
+<li>Chèn MessageBox.js vào website của bạn! <script src="MessageBox.js"></script></li>
+<li>Gọi MessageBox.Show('Nội dung muốn hiển thị',[MessageBoxButton],[MessageBox.type],[callback])
+	<ul>Trong đó
+		<li>MessageBoxButton là loại button muốn hiển thị trên MessageBox. Có hai loại đó là MessageBoxButton.Ok và MessageBoxButton.YesNo</li>
+		<li>MessageBox.type là kiểu MessageBox có 2 loại đó là MessageBox.error và MessageBox.success</li>
+		<li>callback các hàm thực thi khi bấm nút tương ứng ví dụ muốn hiện thông báo alert khi bấm nút Ok MessageBox.Show('Bấm ok',MessageBoxButton.Ok,MessageBox.success,{Ok: function(){alert('Bạn đã bấm ok');0}}])</li>
+	</ul>
+</li>
+</ol>
---
+++ messagebox.js
@@ -0,0 +1,74 @@
+/**
+ * Created by Dong Hoang on 23/09/2015.
+ */
+MessageBoxButton={Ok: 'btnOk', YesNo: 'btnYesNo',}
+MessageBox={
+    config:{
+        lang: 'vi',
+    },
+    language:{
+        vi:{
+            OK: 'Đồng ý',
+            Yes: 'Xác nhận',
+            No: 'Hủy',
+            ErorMsg: 'MessageBox yêu cầu jQuery để hoạt động'
+        },
+        en:{
+            OK: 'OK',
+            Yes: 'Yes',
+            No: 'No',
+            ErorMsg: 'MessageBox require jQuery to run'
+        }
+
+    },
+    error:'ErrorBox',
+    success: 'SuccessBox',
+    Show: function(message,button,type,callback){
+        var string=this.language.vi;
+        if(this.language[this.config['lang']]!=null) var string=this.language[this.config['lang']];
+        var html='<div id="messagebox_mask" style="position: fixed; z-index: 9999; top: 0px; left: 0px; background: rgba(0, 0, 0, 0.53) none repeat scroll 0% 0%; width: 100%; height: 100%;">';
+        if(type==this.error){
+            html+='<div id="messagebox" style="height: 100px; background:rgba(255, 255, 255, 0.78) none repeat scroll 0% 0%; position: relative; top: 40%; left: 35%; width: 400px; font-style: italic; font-weight: 600; padding: 5px; border: 1px solid rgb(255, 1, 16);">';
+        }
+        else{
+            html+='<div id="messagebox" style="height: 100px; background: rgba(255, 255, 255, 0.78) none repeat scroll 0% 0%; position: relative; top: 40%; left: 35%; width: 400px; font-style: italic; font-weight: 600; padding: 5px; border: 1px solid rgb(0, 128, 0);">';
+        }
+        html+='<div id="message" style="max-height: position: relative; font-size: 13px; font-style: normal; font-weight: 400;">'+message+'</span>';
+        if(button==MessageBoxButton.Ok){
+            html+='<div style="position: absolute; color: white; background-color: green; width: 65px; padding: 4px; left: 165px; text-align: center; cursor: pointer; top: 65px; border: 2px solid white;" id="messagebox-btn-ok" title="'+string.OK+'">'+string.OK+'</div>';
+        }
+        if(button==MessageBoxButton.YesNo){
+            html+='<span style="position: absolute; color: white; background-color: green; width: 65px; padding: 4px; left: 130px; text-align: center; border: 2px solid white; cursor: pointer; top: 65px; font-style: normal;" id="messagebox-btn-yes" title="'+string.Yes+'">'+string.Yes+'</span>';
+            html+='<span style="color: white; background-color: rgb(251, 3, 3); width: 65px; padding: 4px; position: absolute; left: 212px; text-align: center; border: 2px solid white; cursor: pointer; top: 65px; font-style: normal;" id="messagebox-btn-no" title="'+string.No+'">'+string.No+'</span>';
+        }
+        html+='</div></div>';
+        basediv=document.createElement('div');
+        basediv.id="messagebox_base";
+        basediv.innerHTML=html;
+        document.body.insertBefore(basediv,document.body.firstChild);
+        document.body.style.overflow="hiden";
+        if(callback==null) callback={};
+        var close=function(){
+            document.body.removeChild(document.querySelector('#messagebox_base'));
+            document.body.style.overflow='auto';
+        };
+        if(button==MessageBoxButton.Ok){
+            document.querySelector('#messagebox-btn-ok').onclick=function(){
+                if(callback.Ok!=null) callback.Ok();
+                close();
+            };
+        }
+        if(button==MessageBoxButton.YesNo) {
+            document.querySelector('#messagebox-btn-yes').onclick = function () {
+                if (callback.Yes != null) callback.Yes();
+                close();
+            };
+            document.querySelector('#messagebox-btn-no').onclick = function () {
+                if (callback.No != null) callback.No();
+                close();
+            };
+        }
+    }
+
+}
+
